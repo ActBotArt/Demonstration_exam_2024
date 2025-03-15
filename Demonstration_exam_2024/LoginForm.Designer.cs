3bfdf0be3bba36c521e09dbefcfae7ed3e9275b0
ACT <Solovjev.artem2003@yandex.ru>
2025-03-15 12:07:22 +0300

    Добавьте файлы проекта.

---
+++ Demonstration_exam_2024/LoginForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Demonstration_exam_2024
+{
+    partial class LoginForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelBackground = new System.Windows.Forms.Panel();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.panelBackground.SuspendLayout();
+            this.SuspendLayout();
+
+            // panelBackground
+            this.panelBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBackground.Controls.Add(this.pictureBoxLogo);
+            this.panelBackground.Controls.Add(this.txtUsername);
+            this.panelBackground.Controls.Add(this.txtPassword);
+            this.panelBackground.Controls.Add(this.btnLogin);
+            this.panelBackground.Controls.Add(this.lblUsername);
+            this.panelBackground.Controls.Add(this.lblPassword);
+
+            // pictureBoxLogo
+            this.pictureBoxLogo.Location = new System.Drawing.Point(100, 20);
+            this.pictureBoxLogo.Size = new System.Drawing.Size(200, 100);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+
+            // lblUsername
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(50, 150);
+            this.lblUsername.Text = "Логин:";
+
+            // txtUsername
+            this.txtUsername.Location = new System.Drawing.Point(130, 147);
+            this.txtUsername.Size = new System.Drawing.Size(200, 23);
+
+            // lblPassword
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(50, 180);
+            this.lblPassword.Text = "Пароль:";
+
+            // txtPassword
+            this.txtPassword.Location = new System.Drawing.Point(130, 177);
+            this.txtPassword.Size = new System.Drawing.Size(200, 23);
+            this.txtPassword.PasswordChar = '●';
+
+            // btnLogin
+            this.btnLogin.Location = new System.Drawing.Point(130, 220);
+            this.btnLogin.Size = new System.Drawing.Size(200, 35);
+            this.btnLogin.Text = "Войти";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+
+            // LoginForm
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.panelBackground);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.panelBackground.ResumeLayout(false);
+            this.panelBackground.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Panel panelBackground;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
---
+++ Demonstration_exam_2024/LoginForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Demonstration_exam_2024
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            SetupForm();
+        }
+
+        private void SetupForm()
+        {
+            try
+            {
+                // Настройка формы
+                this.Text = "Авторизация - Мастер пол";
+
+                // Загрузка иконки из файла
+                this.Icon = new Icon("Resources/Мастер_пол.ico");
+
+                this.BackColor = Color.White;
+                this.Font = new Font("Segoe UI", 9F);
+
+                // Загрузка изображения из файла
+                pictureBoxLogo.Image = Image.FromFile("Resources/Мастер_пол.png");
+                pictureBoxLogo.SizeMode = PictureBoxSizeMode.Zoom;
+
+                // Настройка цветов
+                panelBackground.BackColor = ColorTranslator.FromHtml("#F4E8D3");
+                btnLogin.BackColor = ColorTranslator.FromHtml("#67BA80");
+                btnLogin.ForeColor = Color.White;
+                btnLogin.FlatStyle = FlatStyle.Flat;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке ресурсов: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Здесь будет проверка логина и пароля через базу данных
+            if (username == "admin" && password == "admin") // Временно для тестирования
+            {
+                MainForm mainForm = new MainForm();
+                this.Hide();
+                mainForm.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверные учетные данные", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}

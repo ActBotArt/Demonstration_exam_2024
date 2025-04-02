0fe50431ba20a35e5197ffad090f4218d73c2da8
ACT <Solovjev.artem2003@yandex.ru>
2025-04-02 11:33:52 +0300

    Dell

--- Demonstration_exam_2024/Models/MaterialType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Demonstration_exam_2024.Models
-{
-    public class MaterialType
-    {
-        public int MaterialTypeId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
